ca69c0203951f68eddb61ab8818ef6182b18dd12
stefanschachner <s.schachner@htblaleonding.onmicrosoft.com>
2025-12-16 09:29:36 +0100

    feat: implement sinus service

---
+++ backend/sinus-generator/src/main/java/at/htl/model/SinusData.java
@@ -0,0 +1,4 @@
+package at.htl.model;
+
+public record SinusData(long timestamp, double angle, double value) {
+}
---
+++ backend/sinus-generator/src/main/java/at/htl/service/SinusService.java
@@ -0,0 +1,23 @@
+package at.htl.service;
+
+
+import at.htl.model.SinusData;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class SinusService {
+    private double angle = 0;
+
+    public SinusData next() {
+        double value = Math.sin(Math.toRadians(angle));
+        SinusData data = new SinusData(
+                System.currentTimeMillis(),
+                angle,
+                value
+        );
+
+
+        angle = (angle + 10) % 360;
+        return data;
+    }
+}
