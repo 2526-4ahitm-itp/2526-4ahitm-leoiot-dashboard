dc617cf155c0d97b78e9c91d13b2fed27536bf2e
JonaLeitner <jonas08.leitner@gmail.com>
2026-01-27 07:30:29 +0100

    feat: it works

--- backend/sinus-generator/stop.sh
+++ backend/sinus-generator/stop.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
+#
+#echo "=== Stopping Quarkus (if running) ==="
+#pkill -f quarkus || true
+#
+#echo "=== Stopping Mosquitto ==="
+#pkill -f mosquitto || true
+#
+#echo "=== Stopping InfluxDB ==="
+#pkill -f influxd || true
+#
+#echo "=== Stopping Telegraf ==="
+#sudo systemctl stop telegraf
+#
+#echo "=== Stopping Grafana ==="
+#sudo systemctl stop grafana.service
+#
+#echo "=== All services stopped ==="
 
-echo "=== Stopping Quarkus (if running) ==="
-pkill -f quarkus || true
-
-echo "=== Stopping Mosquitto ==="
-pkill -f mosquitto || true
-
-echo "=== Stopping InfluxDB ==="
-pkill -f influxd || true
-
-echo "=== Stopping Telegraf ==="
-sudo systemctl stop telegraf
-
-echo "=== Stopping Grafana ==="
-sudo systemctl stop grafana.service
-
-echo "=== All services stopped ==="
-
-#docker compose stop
+docker compose stop
